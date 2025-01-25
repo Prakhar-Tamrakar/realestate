25bbfdedb2df6ae997871ff8c6486cd25a0f95ab
Prakhar-Tamrakar <prakhar3091@gmail.com>
2025-01-25 17:37:05 +0530

    create a test api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,3 +19,5 @@ const userSchema =new  mongoose.Schema({
 
 const User = mongoose.model('User' , userSchema);
 
+export default User;  // this will be used in our routes to interact with the database.  // this file should be imported into the server.js file.  // This file should be created in the models folder.  // this file should be created in the models folder.  // this file should be created in the models folder.  // this file should be created in the models folder.  // this file should be created in the models folder.  // this file should
+
