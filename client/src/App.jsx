d9b0e7a5da711888e13a3c153ea848b2cd44a931
Prakhar-Tamrakar <prakhar3091@gmail.com>
2025-07-22 17:59:47 +0530

    final updated repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/newUpdateListing";
+import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 
 function App() {
