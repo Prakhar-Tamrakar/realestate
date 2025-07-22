67f738f840320e51fbbf6972838182a5e7ae97db
Prakhar-Tamrakar <prakhar3091@gmail.com>
2025-07-22 17:40:18 +0530

    managing git conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/UpdateListing";
+import UpdateListing from "./pages/newUpdateListing";
 import Listing from "./pages/Listing";
 
 function App() {
